8581e8a6ac43528f5d7d3bb03eac2e44bd85e345
Cong Dinh <congdinh2021@gmail.com>
2021-08-10 16:25:46 +0700

    BuildCategoryServices

---
+++ src/FA.BookStore/FA.BookStore.Services/CategoryServices.cs
@@ -0,0 +1,14 @@
+﻿using FA.BookStore.Data.Infrastructure;
+using FA.BookStore.Models;
+using FA.BookStore.Services.BaseServices;
+
+namespace FA.BookStore.Services
+{
+    public class CategoryServices : BaseServices<Category>, ICategoryServices
+    {
+        public CategoryServices(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
+
---
+++ src/FA.BookStore/FA.BookStore.Services/ICategoryServices.cs
@@ -0,0 +1,10 @@
+﻿using FA.BookStore.Models;
+using FA.BookStore.Services.BaseServices;
+
+namespace FA.BookStore.Services
+{
+    public interface ICategoryServices : IBaseService<Category>
+    {
+    }
+}
+
