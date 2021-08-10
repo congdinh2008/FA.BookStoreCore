451440677512ffabb1535fd86590faaea3692f3a
Cong Dinh <congdinh2021@gmail.com>
2021-08-10 16:18:37 +0700

    BuildRepositoriesAndUnitOfWork

---
+++ src/FA.BookStore/FA.BookStore.Data/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using FA.BookStore.Data.Infrastructure.BaseRepositories;
+using FA.BookStore.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace FA.BookStore.Data.Infrastructure
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        BookStoreDbContext DataContext { get; }
+
+        #region Master Data
+
+        IGenericRepository<Category> CategoryRepository { get; }
+
+        #endregion
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+
+        IGenericRepository<T> GenericRepository<T>() where T : BaseEntity;
+    }
+}
---
+++ src/FA.BookStore/FA.BookStore.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using FA.BookStore.Data.Infrastructure.BaseRepositories;
+using FA.BookStore.Models;
+using System.Threading.Tasks;
+
+namespace FA.BookStore.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BookStoreDbContext _dbContext;
+
+        public BookStoreDbContext DataContext => _dbContext;
+
+        public UnitOfWork(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+
+        private IGenericRepository<Category> _categoryRepository;
+
+        public IGenericRepository<Category> CategoryRepository =>
+            _categoryRepository ?? new GenericRepository<Category>(_dbContext);
+
+        #region Methods
+        public int SaveChanges()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public IGenericRepository<T> GenericRepository<T>() where T : BaseEntity
+        {
+            return new GenericRepository<T>(_dbContext);
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+        #endregion
+    }
+}
